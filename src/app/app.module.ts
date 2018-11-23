6b4886cf33cd2edf7f016475949c464bb38fd323
Fabio <oliveira_fabio@id.uff.br>
2018-11-22 23:16:47 -0200

    criando component menu-item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,8 +15,9 @@ import { RestaurantsService } from './restaurants/restaurants.service'
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component'
 import { MenuComponent } from './restaurant-detail/menu/menu.component'
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component'
-import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
-import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component'
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
+
 @NgModule({
   declarations: [
     AppComponent,
