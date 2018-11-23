d4447e5e09af0d6e50e33a46db37e430b229adb8
hhesedh <oliveira_fabio@id.uff.br>
2018-11-23 13:26:43 -0200

    Implementando tradução para portugues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser'
-import { NgModule } from '@angular/core'
+import { NgModule, LOCALE_ID } from '@angular/core'
 import { HttpModule } from '@angular/http'
 import { RouterModule } from '@angular/router'
 
@@ -9,6 +9,7 @@ import { HomeComponent } from './home/home.component'
 import { AboutComponent } from './about/about.component'
 
 import { ROUTES } from './app.routes'
+
 import { RestaurantsComponent } from './restaurants/restaurants.component'
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component'
 import { RestaurantsService } from './restaurants/restaurants.service'
@@ -34,7 +35,7 @@ import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-
     ReviewsComponent
   ],
   imports: [BrowserModule, HttpModule, RouterModule.forRoot(ROUTES)],
-  providers: [RestaurantsService, ShoppingCartService],
+  providers: [RestaurantsService, ShoppingCartService, {provide: LOCALE_ID, useValue: 'pt-BR'}],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
