f3aa14cca72c2b6ffce8157037392d116844c06e
hhesedh <oliveira_fabio@id.uff.br>
2018-11-24 12:12:47 -0200

    Criação do component order

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { RestaurantsComponent } from './restaurants/restaurants.component'
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component'
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 import { MenuComponent } from './restaurant-detail/menu/menu.component'
+import { OrderComponent } from './order/order.component'
 
 export const ROUTES: Routes = [
   { path: '', component: HomeComponent },
@@ -19,5 +20,6 @@ export const ROUTES: Routes = [
       { path: 'reviews', component: ReviewsComponent }
     ]
   },
+  { path: 'order', component: OrderComponent},
   { path: 'about', component: AboutComponent }
 ]
