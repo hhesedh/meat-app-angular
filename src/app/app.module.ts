0e0a9e6af3492e567471bf7d9849fe98befbc09a
Fábio <oliveira_fabio@id.uff.br>
2018-12-02 11:12:22 -0200

    Criação do component order-items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,10 +19,11 @@ import { MenuComponent } from './restaurant-detail/menu/menu.component'
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component'
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component'
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
-import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
-import { OrderComponent } from './order/order.component';
-import { InputComponent } from './shared/input/input.component';
-import { RadioComponent } from './shared/radio/radio.component';
+import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service'
+import { OrderComponent } from './order/order.component'
+import { InputComponent } from './shared/input/input.component'
+import { RadioComponent } from './shared/radio/radio.component'
+import { OrderItemsComponent } from './order/order-items/order-items.component'
 
 @NgModule({
   declarations: [
@@ -39,10 +40,20 @@ import { RadioComponent } from './shared/radio/radio.component';
     ReviewsComponent,
     OrderComponent,
     InputComponent,
-    RadioComponent
+    RadioComponent,
+    OrderItemsComponent
   ],
-  imports: [BrowserModule, HttpModule, RouterModule.forRoot(ROUTES), FormsModule],
-  providers: [RestaurantsService, ShoppingCartService, {provide: LOCALE_ID, useValue: 'pt-BR'}],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(ROUTES),
+    FormsModule
+  ],
+  providers: [
+    RestaurantsService,
+    ShoppingCartService,
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
