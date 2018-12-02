678982cabb02a306326ccf844019cbe79b65ab4a
Fábio <oliveira_fabio@id.uff.br>
2018-12-02 11:51:55 -0200

    Fornecendo os itens da compra

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,20 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { RadioOption } from 'app/shared/radio/radio-option.model';
+import { Component, OnInit } from '@angular/core'
+
+import { RadioOption } from 'app/shared/radio/radio-option.model'
+import { OrderService } from './order.service'
+import { CartItem } from './../restaurant-detail/shopping-cart/cart-item.model'
 
 @Component({
   selector: 'mt-order',
   templateUrl: './order.component.html'
 })
 export class OrderComponent implements OnInit {
-
   paymentOptions: RadioOption[] = [
-    {label: 'Dinheiro', value: 'MON'},
-    {label: 'Cartão de Débito', value: 'DEB'},
-    {label: 'Cartão Refeição', value: 'REF'}
+    { label: 'Dinheiro', value: 'MON' },
+    { label: 'Cartão de Débito', value: 'DEB' },
+    { label: 'Cartão Refeição', value: 'REF' }
   ]
-  constructor() { }
+  constructor(private orderService: OrderService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  cartItems() {
+    return this.orderService.CartItems()
   }
 
+  increaseQty(item: CartItem) {
+    this.orderService.increaseQty(item)
+  }
+
+  decreaseQty(item: CartItem) {
+    this.orderService.decreaseQty(item)
+  }
+
+  remove(item: CartItem) {
+    this.orderService.remove(item)
+  }
 }
