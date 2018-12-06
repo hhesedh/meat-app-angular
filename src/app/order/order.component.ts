b6d0ec97edc2a3b918683dbab4735842c659ce36
Fábio <oliveira_fabio@id.uff.br>
2018-12-06 00:43:32 -0200

    Criação de validadores personalizados

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core'
 import { Router } from '@angular/router'
-import { FormGroup, FormBuilder, Validators } from '@angular/forms'
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl
+} from '@angular/forms'
 
 import { RadioOption } from 'app/shared/radio/radio-option.model'
 import { OrderService } from './order.service'
@@ -12,6 +17,8 @@ import { Order, OrderItem } from './order.model'
   templateUrl: './order.component.html'
 })
 export class OrderComponent implements OnInit {
+
+
   emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
 
   numberPattern = /^[0-9]*$/
@@ -24,6 +31,21 @@ export class OrderComponent implements OnInit {
     { label: 'Cartão de Débito', value: 'DEB' },
     { label: 'Cartão Refeição', value: 'REF' }
   ]
+
+  static equalsTo(group: AbstractControl): { [key: string]: boolean } {
+    const email = group.get('email')
+    const emailConfirmation = group.get('emailConfirmation')
+    if (!email || !emailConfirmation) {
+      return undefined
+    }
+
+    if (email.value !== emailConfirmation.value) {
+      return { emailsNotMatch: true }
+    }
+
+    return undefined
+  }
+
   constructor(
     private orderService: OrderService,
     private router: Router,
@@ -31,30 +53,33 @@ export class OrderComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.orderForm = this.formBuilder.group({
-      name: this.formBuilder.control('', [
-        Validators.required,
-        Validators.minLength(5)
-      ]),
-      email: this.formBuilder.control('', [
-        Validators.required,
-        Validators.pattern(this.emailPattern)
-      ]),
-      emailConfirmation: this.formBuilder.control('', [
-        Validators.required,
-        Validators.pattern(this.emailPattern)
-      ]),
-      address: this.formBuilder.control('', [
-        Validators.required,
-        Validators.minLength(5)
-      ]),
-      number: this.formBuilder.control('', [
-        Validators.required,
-        Validators.pattern(this.numberPattern)
-      ]),
-      optionalAddress: this.formBuilder.control(''),
-      paymentOption: this.formBuilder.control('', [Validators.required])
-    })
+    this.orderForm = this.formBuilder.group(
+      {
+        name: this.formBuilder.control('', [
+          Validators.required,
+          Validators.minLength(5)
+        ]),
+        email: this.formBuilder.control('', [
+          Validators.required,
+          Validators.pattern(this.emailPattern)
+        ]),
+        emailConfirmation: this.formBuilder.control('', [
+          Validators.required,
+          Validators.pattern(this.emailPattern)
+        ]),
+        address: this.formBuilder.control('', [
+          Validators.required,
+          Validators.minLength(5)
+        ]),
+        number: this.formBuilder.control('', [
+          Validators.required,
+          Validators.pattern(this.numberPattern)
+        ]),
+        optionalAddress: this.formBuilder.control(''),
+        paymentOption: this.formBuilder.control('', [Validators.required])
+      },
+      { validator: OrderComponent.equalsTo }
+    )
   }
 
   itemsValue(): number {
@@ -87,4 +112,5 @@ export class OrderComponent implements OnInit {
     })
     console.log(order)
   }
+
 }
