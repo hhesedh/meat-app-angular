1f5e35cd4a76c224fa916d25e65ad8f55ecf377b
Fábio <oliveira_fabio@id.uff.br>
2018-12-10 17:05:32 -0200

    Criação de uma feature module para o modulo de compras

---
+++ src/app/order/order.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
+
+import { DeliveryCostsComponent } from './delivery-costs/delivery-costs.component'
+import { OrderItemsComponent } from './order-items/order-items.component'
+import { OrderComponent } from './order.component'
+import { SharedModule } from './../shared/shared.module'
+
+const ROUTES: Routes = [{ path: '', component: OrderComponent }]
+
+@NgModule({
+  declarations: [OrderComponent, OrderItemsComponent, DeliveryCostsComponent],
+  imports: [SharedModule, RouterModule.forChild(ROUTES)]
+})
+export class OrderModule {}
