1ddf60bedb5947c13e4541031464d91b3061e364
Fábio <oliveira_fabio@id.uff.br>
2018-12-10 18:09:46 -0200

    Instalação do módulo de animações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { RouterModule, PreloadAllModules } from '@angular/router'
 import { AppComponent } from './app.component'
 import { HeaderComponent } from './header/header.component'
 import { HomeComponent } from './home/home.component'
-
+import { BrowserAnimationsModule }  from '@angular/platform-browser/animations'
 import { ROUTES } from './app.routes'
 
 import { RestaurantsComponent } from './restaurants/restaurants.component'
@@ -35,6 +35,7 @@ import { SharedModule } from './shared/shared.module'
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpModule,
     SharedModule.forRoot(),
     RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules})
