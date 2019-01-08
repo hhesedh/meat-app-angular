3a0676a0e5ad3b21b61d3474efc6723e36377705
Unknown <oliveira_fabio@id.uff.br>
2019-01-07 22:48:35 -0200

    Criando o component de login

--- src/app/security/login/login.component.ts
+++ src/app/security/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 @Component({
   selector: 'mt-login',
   templateUrl: './login.component.html',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.loginForm = this.fb.group({
+      email: this.fb.control('', [Validators.required, Validators.email]),
+      password: this.fb.control('', [Validators.required]),
+    })
   }
 
 }
